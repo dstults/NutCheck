743b60d61753fa3df3db8a22bfb61091091df0ab
Darren <drankof@gmail.com>
2020-01-08 20:52:00 -0800

    Initial Upload
    Just programmed this for our class assignment today, it has a few bugs in it but for the most part the performance is stable and nice.

---
+++ Nutcheck.Designer.vb
@@ -0,0 +1,281 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Nutcheck
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnTest = New System.Windows.Forms.Button()
+        Me.txtIpAddress = New System.Windows.Forms.TextBox()
+        Me.lblPingReply = New System.Windows.Forms.Label()
+        Me.lblPing = New System.Windows.Forms.Label()
+        Me.txtPort = New System.Windows.Forms.TextBox()
+        Me.lblTCP = New System.Windows.Forms.Label()
+        Me.lblTcpReply = New System.Windows.Forms.Label()
+        Me.lblPort = New System.Windows.Forms.Label()
+        Me.lblIP = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtPingTimeout = New System.Windows.Forms.TextBox()
+        Me.chkPing = New System.Windows.Forms.CheckBox()
+        Me.chkTcp = New System.Windows.Forms.CheckBox()
+        Me.lblAbout = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.txtLog = New System.Windows.Forms.TextBox()
+        Me.chkUdp = New System.Windows.Forms.CheckBox()
+        Me.lblSpinner = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnTest
+        '
+        Me.btnTest.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!)
+        Me.btnTest.Location = New System.Drawing.Point(440, 20)
+        Me.btnTest.Name = "btnTest"
+        Me.btnTest.Size = New System.Drawing.Size(137, 70)
+        Me.btnTest.TabIndex = 0
+        Me.btnTest.Text = "RUN TEST"
+        Me.btnTest.UseVisualStyleBackColor = True
+        '
+        'txtIpAddress
+        '
+        Me.txtIpAddress.Location = New System.Drawing.Point(52, 30)
+        Me.txtIpAddress.Name = "txtIpAddress"
+        Me.txtIpAddress.Size = New System.Drawing.Size(100, 20)
+        Me.txtIpAddress.TabIndex = 1
+        Me.txtIpAddress.Text = "yahoo.com"
+        Me.txtIpAddress.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'lblPingReply
+        '
+        Me.lblPingReply.Location = New System.Drawing.Point(158, 30)
+        Me.lblPingReply.Name = "lblPingReply"
+        Me.lblPingReply.Size = New System.Drawing.Size(100, 20)
+        Me.lblPingReply.TabIndex = 2
+        Me.lblPingReply.Text = "-"
+        Me.lblPingReply.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblPing
+        '
+        Me.lblPing.Location = New System.Drawing.Point(158, 10)
+        Me.lblPing.Name = "lblPing"
+        Me.lblPing.Size = New System.Drawing.Size(100, 20)
+        Me.lblPing.TabIndex = 18
+        Me.lblPing.Text = "PING"
+        Me.lblPing.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'txtPort
+        '
+        Me.txtPort.Location = New System.Drawing.Point(264, 31)
+        Me.txtPort.Name = "txtPort"
+        Me.txtPort.Size = New System.Drawing.Size(67, 20)
+        Me.txtPort.TabIndex = 19
+        Me.txtPort.Text = "80"
+        Me.txtPort.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'lblTCP
+        '
+        Me.lblTCP.Location = New System.Drawing.Point(337, 8)
+        Me.lblTCP.Name = "lblTCP"
+        Me.lblTCP.Size = New System.Drawing.Size(100, 20)
+        Me.lblTCP.TabIndex = 20
+        Me.lblTCP.Text = "TCP"
+        Me.lblTCP.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblTcpReply
+        '
+        Me.lblTcpReply.Location = New System.Drawing.Point(337, 31)
+        Me.lblTcpReply.Name = "lblTcpReply"
+        Me.lblTcpReply.Size = New System.Drawing.Size(100, 20)
+        Me.lblTcpReply.TabIndex = 21
+        Me.lblTcpReply.Text = "-"
+        Me.lblTcpReply.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblPort
+        '
+        Me.lblPort.Location = New System.Drawing.Point(261, 8)
+        Me.lblPort.Name = "lblPort"
+        Me.lblPort.Size = New System.Drawing.Size(70, 20)
+        Me.lblPort.TabIndex = 23
+        Me.lblPort.Text = "PORT"
+        Me.lblPort.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblIP
+        '
+        Me.lblIP.Location = New System.Drawing.Point(52, 8)
+        Me.lblIP.Name = "lblIP"
+        Me.lblIP.Size = New System.Drawing.Size(100, 20)
+        Me.lblIP.TabIndex = 22
+        Me.lblIP.Text = "IP"
+        Me.lblIP.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(5, 76)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(73, 20)
+        Me.Label2.TabIndex = 26
+        Me.Label2.Text = "Ping Timeout:"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'txtPingTimeout
+        '
+        Me.txtPingTimeout.Location = New System.Drawing.Point(84, 78)
+        Me.txtPingTimeout.Name = "txtPingTimeout"
+        Me.txtPingTimeout.Size = New System.Drawing.Size(48, 20)
+        Me.txtPingTimeout.TabIndex = 25
+        Me.txtPingTimeout.Text = "500"
+        Me.txtPingTimeout.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'chkPing
+        '
+        Me.chkPing.AutoCheck = False
+        Me.chkPing.AutoSize = True
+        Me.chkPing.Checked = True
+        Me.chkPing.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.chkPing.Location = New System.Drawing.Point(66, 56)
+        Me.chkPing.Name = "chkPing"
+        Me.chkPing.Size = New System.Drawing.Size(71, 17)
+        Me.chkPing.TabIndex = 27
+        Me.chkPing.Text = "Ping Test"
+        Me.chkPing.UseVisualStyleBackColor = True
+        '
+        'chkTcp
+        '
+        Me.chkTcp.AutoCheck = False
+        Me.chkTcp.AutoSize = True
+        Me.chkTcp.Checked = True
+        Me.chkTcp.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.chkTcp.Location = New System.Drawing.Point(264, 57)
+        Me.chkTcp.Name = "chkTcp"
+        Me.chkTcp.Size = New System.Drawing.Size(71, 17)
+        Me.chkTcp.TabIndex = 28
+        Me.chkTcp.Text = "TCP Test"
+        Me.chkTcp.UseVisualStyleBackColor = True
+        '
+        'lblAbout
+        '
+        Me.lblAbout.AutoSize = True
+        Me.lblAbout.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAbout.Location = New System.Drawing.Point(559, 446)
+        Me.lblAbout.Name = "lblAbout"
+        Me.lblAbout.Size = New System.Drawing.Size(19, 13)
+        Me.lblAbout.TabIndex = 29
+        Me.lblAbout.Text = "@"
+        '
+        'Label4
+        '
+        Me.Label4.Location = New System.Drawing.Point(132, 78)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(24, 20)
+        Me.Label4.TabIndex = 32
+        Me.Label4.Text = "ms"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 10
+        '
+        'txtLog
+        '
+        Me.txtLog.Location = New System.Drawing.Point(10, 113)
+        Me.txtLog.Multiline = True
+        Me.txtLog.Name = "txtLog"
+        Me.txtLog.ReadOnly = True
+        Me.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.txtLog.Size = New System.Drawing.Size(532, 346)
+        Me.txtLog.TabIndex = 35
+        Me.txtLog.Text = "Welcome to Darren's Nutcheck!" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Feel free to contact me at drankof@gmail.com" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'chkUdp
+        '
+        Me.chkUdp.AutoCheck = False
+        Me.chkUdp.AutoSize = True
+        Me.chkUdp.Enabled = False
+        Me.chkUdp.Location = New System.Drawing.Point(264, 76)
+        Me.chkUdp.Name = "chkUdp"
+        Me.chkUdp.Size = New System.Drawing.Size(73, 17)
+        Me.chkUdp.TabIndex = 33
+        Me.chkUdp.Text = "UDP Test"
+        Me.chkUdp.UseVisualStyleBackColor = True
+        '
+        'lblSpinner
+        '
+        Me.lblSpinner.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblSpinner.Location = New System.Drawing.Point(548, 410)
+        Me.lblSpinner.Name = "lblSpinner"
+        Me.lblSpinner.Size = New System.Drawing.Size(23, 23)
+        Me.lblSpinner.TabIndex = 36
+        Me.lblSpinner.Text = "-"
+        Me.lblSpinner.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.lblSpinner.Visible = False
+        '
+        'Nutcheck
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(590, 471)
+        Me.Controls.Add(Me.lblSpinner)
+        Me.Controls.Add(Me.txtLog)
+        Me.Controls.Add(Me.chkUdp)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.lblAbout)
+        Me.Controls.Add(Me.chkTcp)
+        Me.Controls.Add(Me.chkPing)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtPingTimeout)
+        Me.Controls.Add(Me.lblPort)
+        Me.Controls.Add(Me.lblIP)
+        Me.Controls.Add(Me.lblTcpReply)
+        Me.Controls.Add(Me.lblTCP)
+        Me.Controls.Add(Me.txtPort)
+        Me.Controls.Add(Me.lblPing)
+        Me.Controls.Add(Me.lblPingReply)
+        Me.Controls.Add(Me.txtIpAddress)
+        Me.Controls.Add(Me.btnTest)
+        Me.DoubleBuffered = True
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Name = "Nutcheck"
+        Me.Text = "Darren's Nutcheck"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnTest As Button
+    Friend WithEvents txtIpAddress As TextBox
+    Friend WithEvents lblPingReply As Label
+    Friend WithEvents lblPing As Label
+    Friend WithEvents txtPort As TextBox
+    Friend WithEvents lblTCP As Label
+    Friend WithEvents lblTcpReply As Label
+    Friend WithEvents lblPort As Label
+    Friend WithEvents lblIP As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtPingTimeout As TextBox
+    Friend WithEvents chkPing As CheckBox
+    Friend WithEvents chkTcp As CheckBox
+    Friend WithEvents lblAbout As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents txtLog As TextBox
+    Friend WithEvents chkUdp As CheckBox
+    Friend WithEvents lblSpinner As Label
+End Class
---
+++ Nutcheck.vb
@@ -0,0 +1,229 @@
+﻿Public Class Nutcheck
+
+    ' Shared variable(s)
+    Private tgtAddress As New Net.IPAddress(0)
+    Private tgtPort As Integer = 80
+    Private testTcpSocket As Net.Sockets.TcpClient
+
+    Private currentBoredom As Integer = 0
+    Private ReadOnly maxBoredom As Integer = 5 ' seconds until I abort
+
+    Private waitForPing As Boolean
+    Private waitForTCP As Boolean
+    Private waitForUDP As Boolean
+
+    Private Enum TestState
+        idle
+        working
+        finished
+    End Enum
+
+    Private myTestState As Integer = TestState.idle
+
+    Private Sub LogOut(logText As String)
+        If Strings.Right(logText, 3) <> "..." And Strings.Right(logText, 1) <> "!" Then logText = "  " & logText
+        txtLog.Text &= logText & vbNewLine
+        Me.Refresh()
+    End Sub
+
+    Private Sub btnTest_Click(sender As Object, e As EventArgs) Handles btnTest.Click
+        ' RESET & START PROCESS
+        txtLog.Text = ""
+        lblPingReply.Text = "untested"
+        lblTcpReply.Text = "untested"
+        currentBoredom = 0
+        lblSpinner.Visible = True
+        btnTest.Enabled = False
+        Timer1.Enabled = True
+        myTestState = TestState.working
+
+        If chkPing.Checked Or chkTcp.Checked Or chkUdp.Checked Then
+            GetIPAddress()
+            If myTestState = TestState.working Then
+                If chkPing.Checked Then DoPingTest() Else LogOut("Ping test skipped!")
+                If chkTcp.Checked Or chkUdp.Checked Then GetPort()
+                If chkTcp.Checked Then DoTcpTest() Else LogOut("TCP test skipped!")
+                'If chkUdp.Checked Then DoUdpTest() Else LogOut("UDP test skipped!")
+            End If
+        Else
+            LogOut("User attempted test without enabling any tests. Silly user.")
+            MsgBox("You must enable at least one test.", MsgBoxStyle.Critical)
+        End If
+
+    End Sub
+
+    Private Sub GetIPAddress()
+        LogOut("Attempting to read IP address...")
+
+        Try
+            ' Test input for ip-addressyness
+            tgtAddress = Net.IPAddress.Parse(txtIpAddress.Text)
+            If txtIpAddress.Text <> tgtAddress.ToString Then
+                LogOut("IP address was simplified")
+                ' Show user what input was parsed to
+                txtIpAddress.Text = tgtAddress.ToString
+            End If
+            LogOut("IP input accepted: " & tgtAddress.ToString)
+        Catch ex As Exception
+            LogOut("EXCEPTION:  " & ex.Message)
+            Try_DNS()
+        End Try
+
+        LogOut("IP address acquisition finished")
+    End Sub
+
+    Private Sub Try_DNS()
+        LogOut("Attempting DNS process...")
+
+        Dim DNSLookup As New Net.IPHostEntry
+        Try
+            DNSLookup = Net.Dns.GetHostEntry(txtIpAddress.Text)
+        Catch ex As Exception ' Failure to resolve, terminate test prematurely
+            LogOut("EXCEPTION:  " & ex.Message)
+            LogOut("Terminating test prematurely")
+            myTestState = TestState.finished
+        End Try
+
+        If DNSLookup IsNot Nothing Then
+            LogOut("NS record(s) found for: " & txtIpAddress.Text)
+            If DNSLookup.AddressList IsNot Nothing Then
+                For Each ip As Net.IPAddress In DNSLookup.AddressList
+                    LogOut(ip.ToString)
+                Next
+                tgtAddress = DNSLookup.AddressList.First
+                If DNSLookup.AddressList.Count > 1 Then LogOut("More than one DNS entries found, using first: " & tgtAddress.ToString)
+                txtIpAddress.Text = tgtAddress.ToString
+            Else
+                LogOut("No addresses attached to NS record, aborting")
+                myTestState = TestState.finished
+            End If
+        Else
+            LogOut("No addresses found")
+        End If
+
+        LogOut("DNS resolution finished")
+    End Sub
+
+    Private Sub DoPingTest()
+        LogOut("Attempting ping operation...")
+        waitForPing = True
+
+        ' Idiot-proofing
+        Dim myTimeout As Integer = CInt(txtPingTimeout.Text)
+        If myTimeout.ToString <> txtPingTimeout.Text Then
+            ' Show user input was idiot-proofed
+            LogOut("Ping timeout input was changed")
+            txtPingTimeout.Text = myTimeout.ToString
+        End If
+        Dim myPing As New Net.NetworkInformation.Ping
+        Dim pingReply As Net.NetworkInformation.PingReply
+        LogOut("Sending ping (ICMP Echo Request) to " & tgtAddress.ToString)
+        Try
+            'The mid-old way.
+            'If My.Computer.Network.Ping(txtIP1.Text) Then lblPingReply.Text = "Hit!" Else lblPingReply.Text = "Miss!"
+            pingReply = myPing.Send(txtIpAddress.Text, myTimeout)
+            If pingReply.Status = Net.NetworkInformation.IPStatus.Success Then
+                lblPingReply.Text = pingReply.RoundtripTime.ToString & " ms"
+                LogOut("Ping received after " & pingReply.RoundtripTime.ToString & " ms")
+            Else
+                lblPingReply.Text = "no reply"
+                LogOut("There was no reply")
+            End If
+
+        Catch ex As Exception
+            LogOut("EXCEPTION:  " & ex.Message)
+            lblPingReply.Text = "ERROR!"
+        End Try
+
+        waitForPing = False
+        LogOut("Ping test finished")
+    End Sub
+
+    Private Sub GetPort()
+        LogOut("Attempting to read port...")
+
+        ' Test input for portiness
+        tgtPort = CInt(txtPort.Text)
+        ' Test port for idiot-proofing
+        If tgtPort >= 65536 Or tgtPort <= 0 Then
+            tgtPort = 80
+            LogOut("Target port was out of range, setting to HTTP (80)")
+        End If
+        If tgtPort.ToString <> txtPort.Text Or tgtPort >= 65536 Or tgtPort <= 0 Then
+            LogOut("Port integer input was changed")
+            txtPort.Text = tgtPort.ToString
+        End If
+
+        LogOut("Port acquisition finished")
+    End Sub
+
+    Private Sub DoTcpTest()
+        LogOut("Testing TCP connection with " & tgtAddress.ToString & " over port " & tgtPort.ToString)
+        waitForTCP = True
+
+        testTcpSocket = New Net.Sockets.TcpClient
+        Try
+            testTcpSocket.ConnectAsync(tgtAddress, tgtPort)
+        Catch ex As Exception
+            LogOut("EXCEPTION:  " & ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub DoUdpTest()
+        LogOut("Testing port " & tgtPort & " over TCP...")
+        waitForUDP = True
+
+        waitForUDP = False
+        LogOut("UDP test finished")
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Me.Refresh()
+
+        If waitForPing Then
+        End If
+        If waitForTCP Then
+            If testTcpSocket.Connected Then
+                lblTcpReply.Text = "We get signal!"
+                LogOut("Connection made in under " & (1 + currentBoredom) * Timer1.Interval & " ms")
+                ResetTcpTest()
+            ElseIf currentBoredom > maxBoredom * 1000 / Timer1.Interval Then
+                lblTcpReply.Text = "Port closed?"
+                LogOut("TCP test timed out (> " & maxBoredom & " secs)")
+                ResetTcpTest()
+            End If
+        End If
+        If waitForUDP Then
+        End If
+        If Not waitForPing And Not waitForTCP And Not waitForUDP Then
+            'Reset abort to prepare for next test
+            btnTest.Enabled = True
+            Timer1.Enabled = False
+            lblSpinner.Visible = False
+            LogOut(vbNewLine & "Work complete!")
+            myTestState = TestState.finished
+        Else
+            currentBoredom += 1
+            Select Case lblSpinner.Text
+                Case "-" : lblSpinner.Text = "\"
+                Case "\" : lblSpinner.Text = "|"
+                Case "|" : lblSpinner.Text = "/"
+                Case "/" : lblSpinner.Text = "-"
+            End Select
+        End If
+    End Sub
+
+    Private Sub ResetTcpTest()
+        ' RESET SOCKET
+        testTcpSocket.Close()
+        testTcpSocket.Dispose()
+        waitForTCP = False
+        LogOut("TCP test finished")
+    End Sub
+
+    Private Sub lblAbout_Click(sender As Object, e As EventArgs) Handles lblAbout.Click
+        MsgBox("By Darren Stults" & vbNewLine & "1/8/2020")
+    End Sub
+
+End Class
